2832c7d942c020d2dc0d184c11df0f6b76ea135e
Avyukt27 <164896225+Avyukt27@users.noreply.github.com>
2024-09-29 17:14:03 +0530

    Added Js/Ts language support

--- main.py
+++ main.py
@@ -2,19 +2,45 @@ from InquirerPy import inquirer
 from rich.console import Console
 import typer
 import os
-from pathlib import Path
+import subprocess
 
-cwd = Path.cwd()
-console = Console()
+cwd: str = os.getcwd()
+console: Console = Console()
 
 
 def new_dropdown(name: str, options: list[str]):
     return inquirer.select(name, options).execute()
 
 
+def run_language(language: str, dir_name: str):
+    match language.lower():
+        case "python":
+            print("Python!")
+        case "javascript/typescript":
+            os.chdir("..")
+            os.rmdir(dir_name)
+            subprocess.run(f"npm create vite@latest {dir_name}", shell=True, text=True)
+            print("Done")
+                    
+
+
 def create():
-    choice = new_dropdown("Test", ["test", "Test", "rw"])
-    os.mkdir(os.path.join(str(cwd), choice))
+    language: str = new_dropdown("Pick a language", ["Python", "Javascript/Typescript"])
+    choice: str = new_dropdown("Test", ["test", "Test", "rw"])
+    dir_name: str = choice
+    extra_num: int = 0
+
+    while True:
+        try:
+            os.mkdir(os.path.join(cwd, dir_name))
+            break
+        except FileExistsError:
+            dir_name = choice
+            extra_num += 1
+            dir_name += str(extra_num)
+
+    os.chdir(choice)
+    run_language(language, dir_name)
 
 
 if __name__ == "__main__":
